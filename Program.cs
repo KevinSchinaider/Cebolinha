d5d1ba906323fd83d7c350475fbfcc20642cf79d
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-17 10:45:23 -0300

    Solução Exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase1, frase2;
+
+Console.WriteLine("Digite sua flase: ");
+frase1 =  Console.ReadLine()!;
+
+frase2  = frase1
+  .Replace("r", "l")
+  .Replace("R", "L");
+
+Console.WriteLine(frase2);
